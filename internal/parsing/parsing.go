f3bfb084d92043a4103f556fe755a788a367b75c
Linus Buddrus <buddruslinus@gmail.com>
2023-03-28 21:09:43 +0200

    fix normalize logic for ParseColor

--- internal/parsing/parsing.go
+++ internal/parsing/parsing.go
@@ -1,6 +1,7 @@
 package parsing
 
 import (
+	"errors"
 	"fmt"
 	"image/color"
 	"log"
@@ -19,18 +20,21 @@ func ParseColor(hex string) (color.RGBA, error) {
 		clr color.RGBA
 		err error
 	)
+
 	switch len(hex) {
 	case 4:
 		_, err = fmt.Sscanf(hex, "#%1x%1x%1x", &clr.R, &clr.G, &clr.B)
-		clr.R <<= 4
-		clr.G <<= 4
-		clr.B <<= 4
+		clr.R |= clr.R << 4
+		clr.G |= clr.G << 4
+		clr.B |= clr.B << 4
 		clr.A = 0xff
 	case 7:
 		_, err = fmt.Sscanf(hex, "#%02x%02x%02x", &clr.R, &clr.G, &clr.B)
 		clr.A = 0xff
 	case 9:
 		_, err = fmt.Sscanf(hex, "#%02x%02x%02x%02x", &clr.R, &clr.G, &clr.B, &clr.A)
+	default:
+		err = errors.New("unexpected format")
 	}
 	if err != nil {
 		return clr, fmt.Errorf("could not parse color %s: %w", hex, err)
